fadd577951e2a49d1da136a92d82374ddc67ba42
MaxKuo <max2571112@gmail.com>
2024-02-11 01:43:24 +0800

    practice interface - print shape

---
+++ Interface_printShape/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	T := triangle{
+		height: 5,
+		base:   10,
+	}
+
+	S := square{
+		sideLength: 4,
+	}
+	printArea(T)
+	printArea(S)
+}
+
+func (t triangle) getArea() float64 {
+	return (t.base * t.height) / 2
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
