4747acbf1048c72d9b9236801ed9b1f897d5e71d
MaxKuo <max2571112@gmail.com>
2024-02-11 22:59:09 +0800

    update comment

--- Interface_ReadAndPrint/main.go
+++ Interface_ReadAndPrint/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	// #step1 read a file from Args provied by user
+	// #step1 open a file from Args provied by user
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
+	// #step2 bcz return value f is File type, and it implement Reader interface, so we can pass it to Copy directly
 	io.Copy(os.Stdout, f)
 }
